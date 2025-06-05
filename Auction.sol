e69372044970e0c43a18303d6871a5d6238237bf
Robert Rondon <robrondon11@gmail.com>
2025-06-05 10:23:31 -0300

    Adding some state variables

--- Auction.sol
+++ Auction.sol
@@ -6,4 +6,9 @@ contract Auction {
     uint256 public constant COMMISSION_RATE = 2; // 2%
     uint256 public constant MIN_BID_INCREMENT = 5; // 5%
     uint256 public constant TIME_EXTENSION = 10 minutes;
+
+    // State variables
+    bool public auctionEnded;
+    uint256 public auctionEndTime;
+    address public owner;
 }
